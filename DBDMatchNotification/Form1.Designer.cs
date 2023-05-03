3f2f88763fe2dce1d49b0dee6a3c9b729b6eeca8
Wojciech Sobolewski <wsobolewski@hotmail.com>
2023-05-03 19:16:55 +0200

    Commit

---
+++ DBDMatchNotification/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace DbDMatchNotification
+
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.menuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.menuItemClose = new System.Windows.Forms.ToolStripMenuItem();
+            this.autostartcheckBox = new System.Windows.Forms.CheckBox();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.ContextMenuStrip = this.menuStrip;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "DBD Match Notification";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.Click += new System.EventHandler(this.notifyIcon1_Click);
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripSeparator1,
+            this.toolStripSeparator2,
+            this.menuItemClose});
+            this.menuStrip.Name = "contextMenuStrip1";
+            this.menuStrip.Size = new System.Drawing.Size(115, 64);
+            this.menuStrip.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip_ItemClicked);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(114, 24);
+            this.toolStripMenuItem1.Text = "Open";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(111, 6);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(111, 6);
+            // 
+            // menuItemClose
+            // 
+            this.menuItemClose.Name = "menuItemClose";
+            this.menuItemClose.Size = new System.Drawing.Size(114, 24);
+            this.menuItemClose.Text = "Close";
+            // 
+            // autostartcheckBox
+            // 
+            this.autostartcheckBox.AutoSize = true;
+            this.autostartcheckBox.Location = new System.Drawing.Point(33, 31);
+            this.autostartcheckBox.Name = "autostartcheckBox";
+            this.autostartcheckBox.Size = new System.Drawing.Size(142, 20);
+            this.autostartcheckBox.TabIndex = 3;
+            this.autostartcheckBox.Text = "Start with Windows ";
+            this.autostartcheckBox.UseVisualStyleBackColor = true;
+            this.autostartcheckBox.CheckStateChanged += new System.EventHandler(this.autostartcheckBox_CheckStateChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(230, 86);
+            this.Controls.Add(this.autostartcheckBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "DBD Match notification";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem menuItemClose;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.CheckBox autostartcheckBox;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+    }
+}
+
---
+++ DBDMatchNotification/Form1.cs
@@ -0,0 +1,163 @@
+﻿using IpHlpApidotnet;
+using Microsoft.Win32;
+using System;
+using System.Windows.Forms;
+
+namespace DbDMatchNotification
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        TCPUDPConnections ts;
+        bool tool_tip = false;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                using (RegistryKey Key = Registry.CurrentUser.OpenSubKey
+                            ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", false))
+                {
+                    object a = Key.GetValue("DbDMatchNotification");
+                    if (a != null)
+                    {
+                        autostartcheckBox.Checked = true;
+                        this.WindowState = FormWindowState.Minimized;
+                        this.Hide();
+                    }
+                    else
+                    {
+                        this.Activate();
+                        this.Show();
+                        this.WindowState = FormWindowState.Normal;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!");
+            }
+            detect();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                this.WindowState = FormWindowState.Minimized;
+                this.Hide();
+            }
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+
+            this.Activate();
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void menuStrip_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            if (e.ClickedItem.Text.Equals("Close"))
+            {
+                if (ts != null)
+                {
+                    ts.StopAutoRefresh();
+
+                    ts.ItemInserted -= Ts_ItemInserted;
+                    ts.ItemDeleted -= Ts_ItemDeleted;
+                };
+                Application.Exit();
+            }
+            else if (e.ClickedItem.Text.Equals("Open"))
+            {
+                this.Activate();
+                this.Show();
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+
+        private void notifyIcon1_Click(object sender, EventArgs e)
+        {
+            menuStrip.Show(Cursor.Position);
+        }
+
+        private void autostartcheckBox_CheckStateChanged(object sender, EventArgs e)
+        {
+            if (autostartcheckBox.Checked)
+            {
+                using (RegistryKey Key = Registry.CurrentUser.OpenSubKey
+                        ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true))
+                {
+                    try
+                    {
+                        Key.SetValue("DbDMatchNotification", Application.ExecutablePath);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Error!");
+                    }
+                }
+            }
+            else
+            {
+                using (RegistryKey Key = Registry.CurrentUser.OpenSubKey
+                        ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true))
+                {
+                    try
+                    {
+                        Key.DeleteValue("DbDMatchNotification");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Error!");
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Match detection
+        /// </summary>
+        private void detect()
+        {
+            if (ts == null)
+            {
+                ts = new TCPUDPConnections();
+            };
+            ts.AutoRefresh = true;
+            ts.StartAutoRefresh();
+            ts.ItemInserted += Ts_ItemInserted;
+            ts.ItemDeleted += Ts_ItemDeleted;
+        }
+
+        private void Ts_ItemInserted(object sender, TCPUDPConnection item, int Position)
+        {
+            if (item.ProcessName.Contains("DeadByDaylight"))
+            {
+                if (item.Protocol.Equals(Protocol.UDP) && !tool_tip)
+                {
+                    tool_tip = true;
+                    this.notifyIcon1.ShowBalloonTip(20, "Dead By Daylight", "Found a match", ToolTipIcon.Info);
+                }
+            }
+        }
+
+        private void Ts_ItemDeleted(object sender, TCPUDPConnection item, int Position)
+        {
+            if (item.ProcessName.Contains("DeadByDaylight"))
+            {
+                if (!item.Protocol.Equals(Protocol.UDP))
+                {
+                    tool_tip = false;
+                }
+            }
+        }
+
+    }
+}
